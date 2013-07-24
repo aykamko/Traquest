6c4e7853e71c930643d6147a7d84989ad261d8ea
Aleks Kamko <aykamko@gmail.com>
2013-07-24 14:44:58 -0700

    initial commit

---
+++ EventsPlannerTests/EventsPlannerTests.m
@@ -0,0 +1,36 @@
+//
+//  EventsPlannerTests.m
+//  EventsPlannerTests
+//
+//  Created by Aleks Kamko on 7/24/13.
+//  Copyright (c) 2013 FBU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EventsPlannerTests : XCTestCase
+
+@end
+
+@implementation EventsPlannerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
