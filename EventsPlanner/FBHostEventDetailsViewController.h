c593cb206fdeefa13d961edfaae3b25c6a02d29f
anupamurali <anupamurali@college.harvard.edu>
2013-07-29 12:53:15 -0700

    Location Tracker and a few more classes added... incomplete
    Summary:
    Location tracker for guests now works perfectly. I'm in the process of adding the positions of the guests
    of a particular event.
    
    Test Plan: Create events and use them to test whether things are working
    
    Reviewers: harshdeep, aykamko, amurthy
    
    Reviewed By: amurthy
    
    Differential Revision: https://phabricator.fb.com/D904489

---
+++ EventsPlanner/FBHostEventDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBHostEventDetailsViewController.h
+//  EventsPlanner
+//
+//  Created by Anupa Murali on 7/29/13.
+//  Copyright (c) 2013 FBU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBHostEventDetailsViewController : NSObject
+
+@end
