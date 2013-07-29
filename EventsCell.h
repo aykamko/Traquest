9497e5baee66f565fbff31153badcacee88b87a2
xianalice <xian_sun@brown.edu>
2013-07-29 11:06:45 -0700

    subclassed each out for layout purposes

---
+++ EventsCell.h
@@ -0,0 +1,13 @@
+//
+//  EventsCell.h
+//  EventsPlanner
+//
+//  Created by Xian Sun on 7/29/13.
+//  Copyright (c) 2013 FBU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsCell : UITableViewCell
+
+@end
---
+++ EventsCell.m
@@ -0,0 +1,29 @@
+//
+//  EventsCell.m
+//  EventsPlanner
+//
+//  Created by Xian Sun on 7/29/13.
+//  Copyright (c) 2013 FBU. All rights reserved.
+//
+
+#import "EventsCell.h"
+
+@implementation EventsCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
