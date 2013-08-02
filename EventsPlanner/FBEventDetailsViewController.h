0119a5040e08da3c9089af1bda996fb18060e9b5
amurthy <am2275@cornell.edu>
2013-08-02 12:44:24 -0700

    Added refactored eventDetailsViewController changes

---
+++ EventsPlanner/FBEventDetailsViewController.h
@@ -0,0 +1,20 @@
+//
+//  FBEventDetailsViewController.h
+//  EventsPlanner
+//
+//  Created by Ashwin Murthy on 7/25/13.
+//  Copyright (c) 2013 FBU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <Parse/Parse.h>
+
+@interface FBEventDetailsViewController : UIViewController <UIScrollViewDelegate>
+
+- (id)initWithGuestEventDetails:(NSDictionary *)details isHost: (BOOL) isHost;
+
+
+
+@end
