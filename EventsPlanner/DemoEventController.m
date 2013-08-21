67b60ae6f20f7506d0645f1196d5220759fb5321
amurthy <am2275@cornell.edu>
2013-08-21 15:40:27 -0700

    Demo works

--- EventsPlanner/DemoEventController.m
+++ EventsPlanner/DemoEventController.m
@@ -12,6 +12,11 @@
 
 @property (nonatomic, strong) NSString *eventId;
 @property CLLocationCoordinate2D venueLocation;
+@property (nonatomic, strong) NSMutableDictionary *directions;
+@property (nonatomic, strong) NSMutableDictionary *distances;
+@property (nonatomic, strong) NSMutableDictionary *times;
+@property (nonatomic, strong) NSTimer *timer;
+
 
 @end
 
@@ -30,6 +35,11 @@
         PFQuery *fakeEventQuery = [PFQuery queryWithClassName:@"Event"];
         [fakeEventQuery whereKey:@"eventId" equalTo:self.eventId];
         
+        self.directions =  [[NSMutableDictionary alloc] init];
+        self.distances =  [[NSMutableDictionary alloc] init];
+        self.times =  [[NSMutableDictionary alloc] init];
+
+        
         [fakeEventQuery getFirstObjectInBackgroundWithBlock:^(PFObject *fakeEvent, NSError *error) {
             __block PFRelation *fakeEventUsers = [fakeEvent relationforKey:@"DummyRelation"];
             PFQuery *fakeEventUsersQuery = [fakeEventUsers query];
@@ -39,9 +49,22 @@
                 for( PFObject *fakeUser in objects) {
 //                    PFObject *fakeUser = [PFObject objectWithClassName:@"DummyObject"];
 //                    [fakeUser setObject:[NSString stringWithFormat:@"%d", rand()] forKey:facebookID];
+                    NSString *fakeUserId = fakeUser[facebookID];
                     
                     double dLat = ((double)(rand()%5000))/10000.0 -0.25;
                     double dLon = ((double)(rand()%5000))/10000.0 -0.25;
+                    
+                    double length = hypot(dLon, dLat);
+                    
+                    int time = rand()%30 + 10;
+                    [self.times setObject:[NSNumber numberWithInt:time] forKey:fakeUserId];
+                    
+                    [self.distances setObject:[NSNumber numberWithDouble:length] forKey:fakeUserId];
+                    
+                    [self.directions setObject:[NSNumber numberWithDouble:(-dLat/length)] forKey:[NSString stringWithFormat:@"Lat%@",fakeUserId]];
+                    [self.directions setObject:[NSNumber numberWithDouble:(-dLon/length)] forKey:[NSString stringWithFormat:@"Lon%@",fakeUserId]];
+                    NSLog(@"%@",self.directions);
+                    
                     PFGeoPoint *fakeUserLocation = [PFGeoPoint geoPointWithLatitude:self.venueLocation.latitude+dLat longitude:self.venueLocation.longitude+dLon];
                     [fakeUser setObject:fakeUserLocation forKey:locationKey];
                     
@@ -49,18 +72,53 @@
                                                            kTimeKey: [NSNumber numberWithDouble: [[NSDate date] timeIntervalSinceReferenceDate]]};
                     [fakeUser setObject:@[fakeUserLocationDict, fakeUserLocationDict] forKey:kLocationData];
                     
-                    //[fakeUser save];
+                    [fakeUser save];
                     
                     [fakeEventUsers addObject:fakeUser];
+                    
                 }
-                
-                [fakeEvent saveInBackground];
+                self.timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(movePoints) userInfo:nil repeats:YES];
+                [self.timer fire];
+                NSLog(@"%@",self.directions);
             }];
             
         }];
         
     }
     return self;
+}
+
+- (void)movePoints {
+    PFQuery *dummyObjectQuery = [PFQuery queryWithClassName:@"DummyObject"];
+    PFGeoPoint *venueGeoPoint = [PFGeoPoint geoPointWithLatitude:self.venueLocation.latitude longitude:self.venueLocation.longitude];
+    [dummyObjectQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        
+        for (PFObject *dummyObject in objects) {
+            NSString *fakeId = [dummyObject objectForKey:facebookID];
+            NSArray *locationsArray = [dummyObject objectForKey:kLocationData];
+            NSDictionary *locationDataDict = [locationsArray objectAtIndex:1];
+            PFGeoPoint *currentLocation = locationDataDict[locationKey];
+            
+            if ([currentLocation distanceInMilesTo:venueGeoPoint]<0.5) {
+                continue;
+            }
+            NSNumber *distance = self.distances[fakeId];
+            NSNumber *timeSteps = self.times[fakeId];
+            double magnitude = [distance doubleValue]/[timeSteps doubleValue];
+            
+            double deltaLat = [self.directions[[NSString stringWithFormat:@"Lat%@",fakeId ]] doubleValue]*magnitude;
+            double deltaLon = [self.directions[[NSString stringWithFormat:@"Lon%@",fakeId ]] doubleValue]*magnitude;
+            PFGeoPoint *newLocation = [PFGeoPoint geoPointWithLatitude:currentLocation.latitude+deltaLat
+                                                             longitude:currentLocation.longitude+deltaLon];
+            [dummyObject setObject:newLocation forKey:locationKey];
+            
+            NSDictionary *newDict = @{ locationKey: newLocation,
+                                       kTimeKey: [NSNumber numberWithDouble:[[NSDate date] timeIntervalSinceReferenceDate]] };
+            locationsArray = @[[locationsArray firstObject], newDict];
+            [dummyObject setObject:locationsArray forKey:kLocationData];
+            [dummyObject saveInBackground];
+        }
+    }];
 }
 
 - (UIViewController *)presentableViewController {
